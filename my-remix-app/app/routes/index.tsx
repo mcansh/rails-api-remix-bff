35a67f81e69e3862b8f5559d8a1f77c2f0a464d2
Logan McAnsh <logan@mcan.sh>
2023-02-17 16:27:45 -0500

    have remix hit rails api
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- my-remix-app/app/routes/index.tsx
+++ my-remix-app/app/routes/index.tsx
@@ -1,32 +1,30 @@
+import * as React from "react";
+import { defer } from "@remix-run/node";
+import { Await, useLoaderData } from "@remix-run/react";
+
+export async function loader() {
+  let status = fetch("http://rails:3000/healthcheck").then((res) =>
+    res.json()
+  ) as Promise<{
+    status: "ok";
+  }>;
+  return defer({ status });
+}
+
 export default function Index() {
+  let data = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+
+      <div>
+        Rails API Status:{" "}
+        <React.Suspense fallback={<span>Loading...</span>}>
+          <Await resolve={data.status}>
+            {({ status }) => <span>{status}</span>}
+          </Await>
+        </React.Suspense>
+      </div>
     </div>
   );
 }
